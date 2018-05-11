8fd5a15d06874bab01675ab543146612350eb59b
RJ Simpson <silver911r@me.com>
2018-05-11 16:42:58 -0400

    Implemented Mongo DB to Store Message Objects

--- server.js
+++ server.js
@@ -3,31 +3,45 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hi'},
-]
+var dbUrl = 'mongodb://user:user@ds161175.mlab.com:61175/learning-node-rls'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages)=>{
+        res.send(messages)
+    } )
+    
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+
+    message.save((err)=>{
+        if(err) { sendStatus(500)}
+
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
 })
 
 io.on('connection', (socket)=>{
     console.log('user connected')
 })
 
-
+mongoose.connect(dbUrl, (err) =>{
+    console.log('mongo db connection', err)
+})
 var server = http.listen(3000, () => {
     console.log('server is listeing on port', server.address().port);
 });
